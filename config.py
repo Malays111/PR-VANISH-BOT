938d5d19cd6ecfde2d4dba7a984bfd12bc359a46
Malays111 <Dzagurov121@gmail.com>
2025-09-27 23:20:17 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не установлен в переменных окружения")
+
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
+if not ADMIN_ID:
+    raise ValueError("ADMIN_ID не установлен в переменных окружения")
+
 PHOTO_URL = os.environ.get("PHOTO_URL")
+if not PHOTO_URL:
+    raise ValueError("PHOTO_URL не установлен в переменных окружения")
+
 CRYPTOBOT_TOKEN = os.environ.get("CRYPTOBOT_TOKEN")
+if not CRYPTOBOT_TOKEN:
+    raise ValueError("CRYPTOBOT_TOKEN не установлен в переменных окружения")
+
 TON_API_KEY = os.environ.get("TON_API_KEY", "")
