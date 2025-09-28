2a5e7f263568a8671de4816fe48a40b1e562f10a
Malays111 <Dzagurov121@gmail.com>
2025-09-28 21:31:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8398207757:AAFXYadapvm4avKpWs0M1IPbSTrMP55fTAA"
+BOT_TOKEN = "8490824549:AAF0aRWFbssj2OhIeSyUP1R_uXEHuxl7L6Y"
 ADMIN_ID = 8217088275
 PHOTO_URL = "https://www.dropbox.com/scl/fi/w58dqa8v3gypoyx26qgno/photo_2025-09-27_01-31-37-1.jpg?rlkey=s17vgnm63nu209hs7or0zilxr&st=9fejuodz&dl=1"
 CRYPTOBOT_TOKEN = "463340:AA7AujJqzBp7kIXfSpxXcGyaQtMXbgk8B4E"
